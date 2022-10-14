06486bed061f2d7b963e85869a2c2440e2ff0c22
Dynamo Denis Mbugua <dmbugua66@gmail.com>
2022-10-14 10:34:44 +0300

    server-error

--- AppFrontend/src/app/app-routing.module.ts
+++ AppFrontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 const routes: Routes = [
-  {path:'/not-found', component: NotFoundComponent}
+  {path:'/not-found', component: NotFoundComponent},
+  {path:'/server-error', component: ServerErrorComponent},
 ];
 
 @NgModule({
