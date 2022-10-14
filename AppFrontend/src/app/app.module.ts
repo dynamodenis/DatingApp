cb5e1fe6752cffab0a60b98fb97068cd481462f3
Dynamo Denis Mbugua <dmbugua66@gmail.com>
2022-10-14 10:02:33 +0300

    fixed interceptor

--- AppFrontend/src/app/app.module.ts
+++ AppFrontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestErrorsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
